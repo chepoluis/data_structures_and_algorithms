8172fa03c16c39eb0c6fea8a28b3a24477ff8cd9
Luis <vsjoseluisvs@gmail.com>
2022-11-17 10:52:23 -0600

    unshift method

--- doubly_linked_lists/DoublyLinkedList.js
+++ doubly_linked_lists/DoublyLinkedList.js
@@ -43,6 +43,23 @@ class DoublyLinkedList {
 
         return temp;   
     }
+
+    unshift(value) {
+        const newNode = new Node(value);
+
+        if(this.length === 0) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            newNode.next = this.head;
+            this.head.prev = newNode;
+            this.head = newNode;
+        }
+
+        this.length++;
+
+        return this;
+    }
 }
 
 window.doublyLL = new DoublyLinkedList(1);
