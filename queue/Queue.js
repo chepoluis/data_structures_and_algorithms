dff3744876501f62246540f4a75a752ea1f7bc1f
Luis <vsjoseluisvs@gmail.com>
2022-12-08 11:43:40 -0600

    enqueue method

--- queue/Queue.js
+++ queue/Queue.js
@@ -7,4 +7,22 @@ class Queue {
         this.last = newNode;
         this.length = 1;
     }
+
+    enqueue(value) {
+        const newNode = new Node(value);
+
+        if (this.length === 0) {
+            this.first = newNode;
+            this.last = newNode;
+        } else {
+            this.last.next = newNode;
+            this.last = newNode;
+        }
+
+        this.length++;
+
+        return this;
+    }
 }
+
+window.queue = new Queue(1);
