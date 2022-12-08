181fb14af212e697ee54ac2d9d8ab287ab8b8097
Luis <vsjoseluisvs@gmail.com>
2022-12-08 11:33:36 -0600

    queue constructor

---
+++ queue/Queue.js
@@ -0,0 +1,10 @@
+import { Node } from "./Node.js";
+
+class Queue {
+    constructor(value) {
+        const newNode = new Node(value);
+        this.first = newNode;
+        this.last = newNode;
+        this.length = 1;
+    }
+}
