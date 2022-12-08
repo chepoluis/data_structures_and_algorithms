fa992350a2369d8af8a30760fcb3e9e85d4c6af9
Luis <vsjoseluisvs@gmail.com>
2022-12-08 11:48:39 -0600

    dequeue

--- queue/Queue.js
+++ queue/Queue.js
@@ -23,6 +23,24 @@ class Queue {
 
         return this;
     }
+
+    dequeue() {
+        if (this.length === 0) return undefined;
+        
+        const temp = this.first;
+        
+        if (this.length === 1) {
+            this.first = null;
+            this.last = null;
+        } else {
+            this.first = temp.next;
+            temp.next = null;
+        }
+
+        this.length--;
+
+        return temp;
+    }
 }
 
 window.queue = new Queue(1);
