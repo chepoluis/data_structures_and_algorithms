94360679f05dc5b4352e8aaba3c8924583483812
Luis <vsjoseluisvs@gmail.com>
2022-12-08 11:19:16 -0600

    pop method

--- stack/Stack.js
+++ stack/Stack.js
@@ -21,7 +21,22 @@ class Stack {
         this.length++;
 
         return this;
-    } 
+    }
+
+    pop() {
+        if (this.length === 0) return undefined;
+
+        const temp = this.top;
+
+        this.top = temp.next;
+        temp.next = null;
+
+        this.length--;
+
+        return temp;
+    }
 }
 
 window.stack = new Stack(7);
+stack.push(1);
+stack.push(2);
